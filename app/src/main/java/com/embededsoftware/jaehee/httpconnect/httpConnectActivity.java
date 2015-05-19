5e32320cf8dac3f4c72c5ccf95800dd84a423850
snltt <snltt@snltt.kr>
2015-05-19 22:19:42 +0900

    Version 1.0
    Version 1.0. simple asynch http conn

---
+++ app/src/main/java/com/embededsoftware/jaehee/httpconnect/httpConnectActivity.java
@@ -0,0 +1,116 @@
+package com.embededsoftware.jaehee.httpconnect;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.UnsupportedEncodingException;
+
+
+public class httpConnectActivity extends ActionBarActivity implements View.OnClickListener{
+
+    AsyncHttpClient client = new AsyncHttpClient();
+    TextView m_TV;
+    Button m_btnHttpConn;
+    Button m_btnRst;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_connect);
+
+        m_TV = (TextView)findViewById(R.id.textView);
+        m_btnHttpConn = (Button)findViewById(R.id.httpConnBtn);
+        m_btnRst = (Button)findViewById(R.id.rstBtn);
+
+        m_btnHttpConn.setOnClickListener(this);
+        m_btnRst.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_http_connect, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == m_btnHttpConn)
+        {
+            //client.get(url, AsyncHttpResponseHandler);
+            client.get("http://211.226.92.37:8888", new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+                    // called before request is started
+                    Log.d("state", "onStart");
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                    // called when response HTTP status is "200 OK"
+                    Log.d("state", "onSuccess");
+                    try {
+                        String strRes = new String(response,"UTF-8");
+                        m_TV.setText(strRes);
+                    }
+                    catch (UnsupportedEncodingException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    Log.d("state", "onFailure");
+
+
+                    m_TV.setText(statusCode+"");
+                }
+
+                @Override
+                public void onRetry(int retryNo) {
+                    // called when request is retried
+                    Log.d("state", "onRetry");
+                }
+            });
+        }
+        else if (v==m_btnRst)
+        {
+            m_TV.setText("Hello world!");
+        }
+    }
+}
